780c3b28d6a4099a3ee279a041afe6ee5a16efc0
ezhangy <elizabeth_zhang1@brown.edu>
2023-03-02 00:15:09 -0500

    merge: update index for next migration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import PressRelease from './components/PressRelease';
 import './index.css';
-import ElectionInfoPage from './pages/ElectionInfoPage';
-import FAQPage from './pages/FAQPage';
-import HomePage from './pages/HomePage';
-import PressPage from './pages/PressPage';
-import reportWebVitals from './reportWebVitals';
-import Root from './Root';
-import decFifthPR from './markdown/press-releases/2022-12-05.md';
+import reportWebVitals from '../reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './HomePage';
+import ElectionInfoPage from './ElectionInfoPage';
+import FAQPage from './FAQPage';
+import PressPage from './PressPage';
+import Root from '../Root';
+import PressRelease from '../components/PressRelease';
+import decFifthPR from "./markdown/press-releases/2022-12-05.md";
+
+
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Root />,
-    children: [
-      { index: true, element: <HomePage /> },
-      {
-        path: '/faq',
-        element: <FAQPage />,
-      },
-      {
-        path: '/press',
-        element: <PressPage />,
-      },
-      {
-        path: '/election-info',
-        element: <ElectionInfoPage />,
-      },
-      {
-        path: '/press/releases/2022-12-05',
-        element: (
-          <PressRelease
-            markdownFilePath={decFifthPR}
-            date={'December 5, 2022'}
-          />
-        ),
-      },
-    ],
+      path: "/",
+      element: <Root />,
+      children: [
+          { index: true, element: <HomePage />},
+          {
+            path: "/faq",
+            element: <FAQPage />
+          },
+          {
+            path: "/press",
+            element: <PressPage />
+          },
+          {
+            path: "/election-info",
+            element: <ElectionInfoPage />
+          },
+          {
+            path: "/press/releases/2022-12-05",
+            element: <PressRelease 
+                markdownFilePath={decFifthPR}
+                date={"December 5, 2022"}
+              />
+          }
+      ],
   },
-]);
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+<React.StrictMode>
+  <RouterProvider router={router} />
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
